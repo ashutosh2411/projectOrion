6080a195943eb8b45a099b020d540e259ea2f073
Ashutosh <ashutoshu2411@gmail.com>
2017-09-02 20:00:04 +0530

    implemented svm

--- scripts/svm.py
+++ scripts/svm.py
@@ -6,6 +6,27 @@ from os import listdir
 from os.path import isfile, join
 from sklearn import svm
 
+
+def fun(y_index ,test, training):
+	#test, training = dataset[:1000,:], dataset[1000:,:]
+	
+	clf = svm.SVC()
+	clf.fit(training[:,:5], training[:,y_index])	
+	
+	y_hat = clf.predict(test[:,:5])
+	y = test[:,y_index]
+
+
+	#calculating error
+	error = 0
+	for x in range (len(y_hat)):
+		if y_hat[x] != y[x]:
+			error = error + 1
+	print float(error)/10
+
+
+
+
 x_csv = [f for f in listdir('../datasets_pro') if isfile(join('../datasets_pro', f)) and f.endswith('.csv')]
 for x in x_csv:
 
@@ -14,7 +35,33 @@ for x in x_csv:
 	np.random.shuffle(overall)										# splitting in train and test randomly
 	test, training = overall[:1000,:], overall[1000:,:]
 	
-	clf = svm.SVC()
-	clf.fit(training[:,:6], training[:,7])	
 
-	print clf.predict(test[:,:6]) 
+	y_set = ['opening - opening','closing - closing', 'closing - opening']
+
+	print x
+	for i in [6,7,8] :
+		print y_set[i-6]
+#		print type(overall)
+		fun(int(i), test, training)
+	print ''
+
+
+
+
+	'''
+	#clf = svm.SVC()
+	#clf.fit(training[:,:5], training[:,8])	
+	#print training[:,:7]
+	y_hat = clf.predict(test[:,:5])
+	y = test[:,8]
+	#print y.shape
+	error = 0
+	for x in range (len(y_hat)):
+		if y_hat[x] != y[x]:
+			error = error + 1
+	#print np.dot(clf.predict(test[:,:5]) , test[:,7])
+	print float(error)/10
+
+
+
+	'''
