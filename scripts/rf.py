8937a2f52b2418823cb3f68918884646dfedbbf1
okabe <amishranjanranjan@gmail.com>
2017-09-11 17:21:24 +0530

    Random Forest debugged

---
+++ scripts/rf.py
@@ -0,0 +1,43 @@
+"""
+Program to implement svm on processed data
+"""
+import numpy as np
+from os import listdir
+from os.path import isfile, join
+from sklearn import svm
+import sklearn
+
+x_csv = [f for f in listdir('../datasets_pro') if isfile(join('../datasets_pro', f)) and f.endswith('.csv') and f.startswith('svm_')]
+for x in x_csv:
+
+	x_address = '../datasets_pro/' + x
+	overall = np.genfromtxt(x_address, delimiter = ',')
+	np.random.shuffle(overall)										# splitting in train and test randomly
+	test, training = overall[:1000,:], overall[1000:,:]
+	
+
+	y_set = ['opening - opening','closing - closing', 'closing - opening']
+
+	print x
+	for i in [5,6,7] :
+		print y_set[i-5]
+#		print type(overall)
+		funRF(int(i), test, training)
+	print ''
+
+def funRF(y_index ,test, training):
+	#test, training = dataset[:1000,:], dataset[1000:,:]
+	
+	clf = sklearn.ensemble.RandomForestClassifier()
+	clf.fit(training[:,:4], training[:,y_index])	
+	
+	y_hat = clf.predict(test[:,:4])
+	y = test[:,y_index]
+
+
+	#calculating error
+	error = 0
+	for x in range (len(y_hat)):
+		if y_hat[x] != y[x]:
+			error = error + 1
+	print float(error)/10
